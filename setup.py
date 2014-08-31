17fc141d7bb0abf92ffc726b4573ce7de6c01c59
Allan Beaufour <allan@beaufour.dk>
2014-08-31 17:37:35 -0400

    bump to v0.2.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
         return f.read()
 
 setup(name='flickr_download',
-      version='0.2.3',
+      version='0.2.4',
       description='Download a Flickr set',
       long_description=readme(),
       url='https://github.com/beaufour/flickr-download.git',
