a3f241412caff899f184713d203591a591345150
Allan Beaufour <allan@beaufour.dk>
2017-05-13 18:09:48 -0400

    bumps to v0.2.19

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='flickr_download',
-      version='0.2.18',
+      version='0.2.19',
       description='Download photos from Flickr',
       long_description=readme(),
       url='https://github.com/beaufour/flickr-download.git',
