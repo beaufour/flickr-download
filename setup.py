e7ec02deb027cde3674c46c1232287086701fc1c
Allan Beaufour <allan@beaufour.dk>
2020-03-15 12:56:07 -0400

    upgrades all libraries to the newest versions

--- setup.py
+++ setup.py
@@ -6,30 +6,26 @@ def readme():
         return f.read()
 
 
-setup(
-    name="flickr_download",
-    version="0.2.21",
-    description="Download photos from Flickr",
-    long_description=readme(),
-    url="https://github.com/beaufour/flickr-download.git",
-    author="Allan Beaufour, Marian Such",
-    author_email="allan@beaufour.dk, toxygen1@gmail.com",
-    license="Apache",
-    packages=["flickr_download"],
-    install_requires=[
-        "flickr_api==0.5",
-        "python-dateutil == 1.5",
-        "PyYAML>=5.1",
-        "setuptools==44.1.1",
-        "pathvalidate==0.29.1",
-    ],
-    test_suite="tests.get_tests",
-    tests_require=[
-        "unittest2==1.1.0",
-        "attrdict==0.5.1",
-    ],
-    entry_points={
-        "console_scripts": ["flickr_download=flickr_download.flick_download:main"],
-    },
-    zip_safe=False,
-)
+setup(name='flickr_download',
+      version='0.2.21',
+      description='Download photos from Flickr',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/beaufour/flickr-download.git',
+      author='Allan Beaufour',
+      author_email='allan@beaufour.dk',
+      license='Apache',
+      packages=['flickr_download'],
+      install_requires=[
+          'flickr_api >= 0.7.3',
+          'python-dateutil == 2.8.1',
+          'PyYAML >= 5.3',
+      ],
+      test_suite='tests.get_tests',
+      tests_require=[
+          'attrdict == 2.0.1',
+      ],
+      entry_points={
+          'console_scripts': ['flickr_download=flickr_download.flick_download:main'],
+      },
+      zip_safe=False)
