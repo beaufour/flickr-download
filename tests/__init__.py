759000273d54be760323177fdbc99f13800a2af0
Allan Beaufour <allan@beaufour.dk>
2020-03-15 12:57:25 -0400

    fixes so it uses unittest and not unittest2

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
 import os.path
-import unittest2
+import unittest
 
 
 def get_tests():
     start_dir = os.path.dirname(__file__)
-    return unittest2.TestLoader().discover(start_dir, pattern="*.py")
+    return unittest.TestLoader().discover(start_dir, pattern="*.py")
