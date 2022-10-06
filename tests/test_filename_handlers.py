4850ed405fbd05d1f8c57d0eceb98824a040789f
Allan Beaufour <allan@beaufour.dk>
2022-10-06 10:30:54 -0400

    casts test dicts to get rid of type warnings

--- tests/test_filename_handlers.py
+++ tests/test_filename_handlers.py
@@ -1,11 +1,14 @@
+from typing import cast
+
 from attrdict import AttrDict
+from flickr_api.objects import Photo, Photoset
 from flickr_download.filename_handlers import get_filename_handler
 
 
 class TestFilenameHandlers:
     def setup_method(self) -> None:
-        self._pset = AttrDict({"title": "Some Set", "id": 999})
-        self._photo = AttrDict({"title": "Some Photo", "id": 123})
+        self._pset = cast(Photoset, AttrDict({"title": "Some Set", "id": 999}))
+        self._photo = cast(Photo, AttrDict({"title": "Some Photo", "id": 123}))
         self._suffix = ""
 
     def teardown_method(self) -> None:
@@ -13,7 +16,7 @@ class TestFilenameHandlers:
         assert fn(self._pset, self._photo, self._suffix) == "Some Photo"
 
     def test_title_empty_title(self) -> None:
-        photo = AttrDict({"title": "", "id": 192})
+        photo = cast(Photo, AttrDict({"title": "", "id": 192}))
         fn = get_filename_handler("title")
         assert fn(self._pset, photo, self._suffix) == "192"
 
@@ -22,7 +25,7 @@ class TestFilenameHandlers:
         assert fn(self._pset, self._photo, self._suffix) == "Some Photo-123"
 
     def test_title_and_id_empty_title(self) -> None:
-        photo = AttrDict({"title": "", "id": 1389})
+        photo = cast(Photo, AttrDict({"title": "", "id": 1389}))
         fn = get_filename_handler("title_and_id")
         assert fn(self._pset, photo, self._suffix) == "1389"
 
@@ -37,20 +40,20 @@ class TestFilenameHandlers:
         assert fn(self._pset, self._photo, self._suffix) == "Some Photo(1)"
 
         # Ensure no increment on different title
-        photo2 = AttrDict({"title": "Some Other Photo", "id": 124})
+        photo2 = cast(Photo, AttrDict({"title": "Some Other Photo", "id": 124}))
         assert fn(self._pset, photo2, self._suffix) == "Some Other Photo"
 
         # Ensure no increment on same title, but different set
-        pset2 = AttrDict({"title": "Some Other Set", "id": 1000})
+        pset2 = cast(Photoset, AttrDict({"title": "Some Other Set", "id": 1000}))
         assert fn(pset2, self._photo, self._suffix) == "Some Photo"
 
     def test_title_increment_empty_title(self) -> None:
-        photo = AttrDict({"title": "", "id": 175})
+        photo = cast(Photo, AttrDict({"title": "", "id": 175}))
         fn = get_filename_handler("title_increment")
         assert fn(self._pset, photo, self._suffix) == "175"
 
     def test_valid_path(self) -> None:
-        photo = AttrDict({"title": 'fi:l*e/p"a?t>h|', "id": 199})
+        photo = cast(Photo, AttrDict({"title": 'fi:l*e/p"a?t>h|', "id": 199}))
 
         fn = get_filename_handler("title")
         assert fn(self._pset, photo, self._suffix) == "file_path"
